59da33baf2e764e7af46e54453ddc1da98231466
aikerim <aygerim.sauletkhan@gmail.com>
2017-12-11 21:01:17 -0800

    travis test config

--- server/routes.js
+++ server/routes.js
@@ -1,4 +1,6 @@
-import { Router} from 'express'; 
+var express = require('express'); 
+var Router = express.Router;
+// import { Router} from 'express'; 
 const router = Router(); 
 router.get('/', (req, res) => { 
     // res.status(200).send(JSON.stringify({'message': 'Hello World!!!'}));
