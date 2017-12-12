a28b5ad277fa67a5e7fd827bcc764b7fb150a10d
aikerim <aygerim.sauletkhan@gmail.com>
2017-12-11 20:31:52 -0800

    hello world

--- test/test.js
+++ test/test.js
@@ -1,10 +1,11 @@
 var supertest = require("supertest");
 var should = require("chai").should();
 var dotenv = require('dotenv')
+var PORT = process.env.PORT || 8080
 // This agent refers to PORT where program is runninng.
 dotenv.config()
 // console.log('environment port: ', process.env.PORT)
-var server = supertest.agent("http://localhost:"+process.env.PORT);
+var server = supertest.agent("http://localhost:"+PORT);
 
 // UNIT test begin
 
@@ -18,7 +19,7 @@ describe("SAMPLE unit test", function () {
         server
             .get("/")
             // .expect("Content-type", /json/)
-            .expect(200, done) // THis is HTTP response
+            .expect(200,done) // THis is HTTP response
     
     });
 
