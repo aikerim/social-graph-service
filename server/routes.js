22eb6638cd3bc031996fb44d3a12edcaf17f5fd4
aikerim <aygerim.sauletkhan@gmail.com>
2017-12-12 09:51:18 -0800

    minor: added router in es6

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,7 @@
 // var express = require('express'); 
 // var Router = express.Router;
-// import { Router} from 'express'; 
-const router = Router(); 
+import { Router} from 'express'; 
+// const router = Router(); 
 router.get('/', (req, res) => { 
     // res.status(200).send(JSON.stringify({'message': 'Hello World!!!'}));
     res.status(200).send({"message": "Hello World!"});
